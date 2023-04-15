fd33f6c3883205169b1825376c0bb74f5c17c717
MrLiu0517 <liuyin70517@163.com>
2023-04-15 13:36:51 +0800

    泛型基础

---
+++ 泛型基础/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace 泛型;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //泛型集合
+        List<string> strings = new List<string> { "大福宝", "大聪明", "小三宝" };
+        foreach (var item in strings)
+        {
+            Console.WriteLine(item);
+        }
+        //泛型字典
+        Dictionary<int, string> directory = new Dictionary<int, string> { };
+        int i = 0;
+        foreach (var item in strings)
+        {
+            directory.Add(i++, item);
+        }
+
+        MyClass<int> myClass = new MyClass<int>(10);
+    }
+}
+
+//泛型类
+class MyClass<T>
+{
+    public T t;
+    public MyClass(T t)
+    {
+        this.t = t;
+        Console.WriteLine(this.t);
+    }
+}
+
